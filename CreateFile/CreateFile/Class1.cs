1236300ced39f000cf8450edc940822c0c508909
Gor777777777 <gorgoar@gmail.com>
2017-01-20 15:10:41 +0400

    сделал изменения

--- CreateFile/CreateFile/Class1.cs
+++ CreateFile/CreateFile/Class1.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CreateFile
 {
-    public class Class1
+    public static class Class1
     {
+        public static void NewTextFile(string path,string text)
+        {
+
+            if (File.Exists(path))
+            {
+                throw new Exception("File exist");
+            }
+            else
+            {
+                StreamWriter sw = new StreamWriter(path);
+                sw.Write(text);
+                sw.Close();
+            }
+        }
+
+        public static void MyDelet(string path)
+        {
+            if(!File.Exists(path))
+            {
+                throw new Exception("Can't find file");
+            }
+            File.Delete(path);
+        }
     }
 }
